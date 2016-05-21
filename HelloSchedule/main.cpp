c52327035d9aa98ca71a8e062ce4f73ab75a9d43
Daibolic <bd285@cornell.edu>
2016-05-21 10:35:00 -0400

    Initial Commit

---
+++ HelloSchedule/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloSchedule
+//
+//  Created by Boliang Dai on 5/21/16.
+//  Copyright © 2016 Boliang Dai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
